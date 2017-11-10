2e65d983d474bf3e9a0c61d7a14e8880936d50ec
_bango_ <logan@swango.xyz>
2017-11-10 07:59:30 -0500

    update s3

--- Update_S3.bat
+++ Update_S3.bat
@@ -1,5 +1,4 @@
 @echo off
-If Not Exist updater.hashfile call certUtil -hashfile config.js md5 > updater.hashfile
 call certUtil -hashfile config.js md5 > updater_temp.hashfile
 call FC updater.hashfile updater_temp.hashfile >NUL && set stash=0 || set stash=1
 del /f updater_temp.hashfile
