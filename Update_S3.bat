b1a9f9f41f4c34d2deb5977ada91b9f0ad8c4824
_bango_ <logan@swango.xyz>
2017-11-10 08:05:27 -0500

    hashfile added, update script changed and working

--- Update_S3.bat
+++ Update_S3.bat
@@ -1,11 +1,14 @@
 @echo off
+If Not Exist updater.hashfile call certUtil -hashfile config.js md5 > updater.hashfile
 call certUtil -hashfile config.js md5 > updater_temp.hashfile
 call FC updater.hashfile updater_temp.hashfile >NUL && set stash=0 || set stash=1
 del /f updater_temp.hashfile
 if "%stash%" == "1" (call git stash)
-pause
+echo S3 Updating...
 call git pull origin master
 call certUtil -hashfile config.js md5 > updater.hashfile
 if "%stash%" == "1" (call git stash apply)
+echo S3 Updating Dependencies...
 call npm update
+echo Done
 pause
