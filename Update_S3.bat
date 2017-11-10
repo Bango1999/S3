3115bd69361aab2ce76cbff37e1177f9fe23e72b
_bango_ <logan@swango.xyz>
2017-11-10 07:51:45 -0500

    update s3 bat

--- Update_S3.bat
+++ Update_S3.bat
@@ -1,5 +1,11 @@
-call git stash
+@echo off
+If Not Exist updater.hashfile call certUtil -hashfile config.js md5 > updater.hashfile
+call certUtil -hashfile config.js md5 > updater_temp.hashfile
+call FC updater.hashfile updater_temp.hashfile >NUL && set stash=0 || set stash=1
+if "%stash%" == "1" (call git stash)
+pause
 call git pull origin master
-call git stash apply
+if "%stash%" == "1" (call git stash apply)
 call npm update
+del /f updater_temp.hashfile
 pause
